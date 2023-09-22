14a3db2abd60f8cde50accea7d77a671cba6ee61
Nicolas Saumont <saumont.nicolas@gmail.com>
2023-09-22 11:55:02 +0200

    feat error message when response not ok on form submit

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,72 +1,47 @@
-console.log('Admin page');
+import { apiBaseUrl, openNotification, closeNotification } from './utilsModule';
 
 function listenUserActions() {
-
   // Submit du login form
-  document.getElementById('#login-btn').addEventListener('submit', submitLogin);
+  document.querySelector('.login-form').addEventListener('submit', submitLogin);
 
+  // Close notification
+  document.querySelectorAll('.delete').forEach((deleteButton) => {
+    deleteButton.addEventListener('click', closeNotification);
+  });
 }
 
-function submitLogin() {
-  console.log('login submited');
+async function submitLogin(event) {
+  event.preventDefault(); // Empeche le refresh de la page
 
-  //   event.preventDefault(); // Empeche le refresh de la page
-  
-  //   // console.log(event.target);
-  //   const addListForm = event.target; // On recup le formulaire
+  const userEmail = document.getElementById('userEmail');
+  const userPassword = document.getElementById('userPassword');
 
-  //   // On crée un nouvel objet FormData
-  //   const formData = new FormData(addListForm);
-  //   // console.log(formData.get('name'));
+  const loginDatas = {
+    userEmail: userEmail.value,
+    userPassword: userPassword.value,
+  };
 
-  //   // On converti le FormData en objet JS "classique" plus facilement manipulable
-  //   const formObject = Object.fromEntries(formData);
-  //   // console.log(formObject);
+  const bodyJSON = JSON.stringify(loginDatas);
 
-  //   //? Ici on va avoir un soucis 400 Bad Request car on n'a pas la propriété position dans formObject
-  //   //? Pour pallier à ça, on va simplement la rajouter et l'initialiser à 0
-  //   //? Le mieux aurait été de gérer ce cas côté backend ! (si on ne recoit pas position, alors on l'initialise à 0)
-  //   formObject.position = 0;
+  console.log(bodyJSON);
 
-  //   // Fonction pour faire le CALL API
-  //   creatList(formObject);
+  try {
+    const response = await fetch(`${apiBaseUrl}admin/login`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: bodyJSON,
+    });
 
-
-
-
-  
-
-  // async function creatList(listObject) {
-  //   // On converti notre object JS en JSON simplement
-  //   // car c'est le JSON qu'on enverra à l'API plus tard
-  //   const bodyJSON = JSON.stringify(listObject);
-
-  //   // On place un try...catch dans le cas où on n'arriverait pas à atteindre l'API
-  //   try {
-  //     // On emet une requete HTTP et on recup la réponse de l'API
-  //     const response = await fetch(`${apiBaseUrl}/lists`, {
-  //       method: 'POST', // Méthode HTTP
-  //       headers: { // En-têtes de requête
-  //         'Content-type': 'application/json; charset=UTF-8' // On spécifit à l'API qu'il va recevoir des données en JSON et encodé en UTF-8
-  //       },
-  //       body: bodyJSON // Corps de la requête : on envoi les data au format JSON
-  //     });
-  //     // console.log(response);
-  
-  //     if (response.ok) {
-  //       const newList = await response.json();
-  //       // On insert dans le DOM la liste fraichement crée
-  //       addListToDom(newList);
-  //     } else {
-  //       // On fait un alerte pour aller vite mais dans la vraie et pour le TP, il faudra quelque chose de plus joli !
-  //       // Par exemple avec : https://bulma.io/documentation/elements/notification/
-  //       alert('Oops petit soucis');
-  //     }
-  //   } catch (error) {
-  //     console.trace(error);
-  //   }
-  // }
-
+    if (response.ok) {
+      console.log("L'utilisateur a pu se connecter");
+    } else {
+      openNotification();
+    }
+  } catch (error) {
+    console.trace(error);
+  }
 }
 
 // On rend effectif les branchements d'écouteurs d'event
--- assets/js/utilsModule.js
+++ assets/js/utilsModule.js
@@ -1,19 +1,25 @@
-import { deleteSiteDetailsTemplate, deletePictures, deleteSitesListTemplate } from './siteModule.js';
+import {
+  deleteSiteDetailsTemplate,
+  deletePictures,
+  deleteSitesListTemplate,
+} from './siteModule.js';
 
-export const apiBaseUrl = 'https://portfolio-back.nicolassaumont.com/';
-// export const apiBaseUrl = 'http://localhost:3000';
+// export const apiBaseUrl = 'https://portfolio-back.nicolassaumont.com/';
+export const apiBaseUrl = 'http://localhost:3000/';
 
 export async function closeNotification() {
-
-  document.querySelectorAll('.notification').forEach(notification => {
-
+  document.querySelectorAll('.notification').forEach((notification) => {
     notification.classList.add('is-hidden');
+  });
+}
 
+export async function openNotification() {
+  document.querySelectorAll('.notification').forEach((notification) => {
+    notification.classList.remove('is-hidden');
   });
 }
 
 export function showHomepage() {
-
   deleteSiteDetailsTemplate();
 
   deletePictures();
@@ -21,51 +27,36 @@ export function showHomepage() {
   deleteSitesListTemplate();
 
   showProjects();
-  
+
   showImages();
 
   document.querySelector('.home').classList.remove('is-hidden');
 
-  document.querySelectorAll('[slot="site-id"]').forEach(name => {
-
+  document.querySelectorAll('[slot="site-id"]').forEach((name) => {
     name.classList.remove('active');
-
   });
 
-  document.querySelectorAll('.see-all').forEach(name => {
-
+  document.querySelectorAll('.see-all').forEach((name) => {
     name.classList.remove('active');
-
   });
-
 }
 
 export function hideHomepage() {
-
   document.querySelector('.home').classList.add('is-hidden');
-
 }
 
 export function showProjects() {
-
   document.querySelector('.projects.desktop').classList.remove('is-hidden');
-
 }
 
 export function hideProjects() {
-
   document.querySelector('.projects.desktop').classList.add('is-hidden');
-
 }
 
 export function showImages() {
-
   document.querySelector('.images.desktop').classList.remove('is-hidden');
-
 }
 
 export function hideImages() {
-
   document.querySelector('.images.desktop').classList.add('is-hidden');
-
 }
